26c7c24a1c1db2ce7f782e079d220ad9f9254ce1
mmazz <mazzantimatiass@gmail.com>
2023-09-15 17:13:14 -0300

    new random encoding, error still

--- fhe/sealProfile/img/random_diff.py
+++ fhe/sealProfile/img/random_diff.py
@@ -14,6 +14,17 @@ matplotlib.rcParams.update({'font.size': 20})
 plt.rc('xtick',labelsize=16)
 plt.rc('ytick',labelsize=16)
 
+dir = "../logs/log_encode/"
+
+dir_list = os.listdir(dir)
+for (root, dirs, file) in os.walk(dir):
+    for f in file:
+        if '.txt' in f:
+            df = pd.read_csv(dir+f, header=None,  skip_blank_lines=False)
+            df = df.iloc[1:,:]
+
+            encoding = df[df.columns[0]].to_numpy(dtype='float')
+            print(f"{f}: {encoding.mean()}")
 dir = "../logs/log_encrypt/"
 
 dir_list = os.listdir(dir)
@@ -25,4 +36,3 @@ for (root, dirs, file) in os.walk(dir):
 
             encoding = df[df.columns[0]].to_numpy(dtype='float')
             print(f"{f}: {encoding.mean()}")
-
