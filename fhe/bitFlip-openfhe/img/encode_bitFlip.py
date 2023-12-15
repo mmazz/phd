35b81e179f931fefa4f03c15bee3c5f36e6c0603
mmazz <mazzantimatiass@gmail.com>
2023-12-15 10:42:52 -0300

    Multi RNS

--- fhe/bitFlip-openfhe/img/encode_bitFlip.py
+++ fhe/bitFlip-openfhe/img/encode_bitFlip.py
@@ -10,6 +10,8 @@ En lina de comando poner un de los siguientes 4 casos:
     2: Sin NTT
     3: Sin RNS ni NTT
 Si ademas agregamos un 1, esta nos pondra titulos en los graficos y nos ira mostrando por pantalla cada grafico
+
+La cantidad de bits que tiene la encriptacion es diferente en cada caso y por esos esos factores multiplicativos
 '''
 
 
@@ -30,6 +32,9 @@ extra = ""
 ejecute = True
 verbose = 0
 non_RNS = False
+RNS_proportion = 1/2
+RNS_proportion_ActiveRNS = 1/2
+
 
 print(sys.argv[0], sys.argv[1])
 if(len(sys.argv)==1):
@@ -38,6 +43,7 @@ if(len(sys.argv)==1):
     print("1 = OpenFHE with no RNS")
     print("2 = OpenFHE with no NTT")
     print("3 = OpenFHE with no Optimizations")
+    print("4 = OpenFHE with Optimizations multi RNS")
     ejecute  = False
 
 elif(sys.argv[1]==str(0)):
@@ -46,7 +52,7 @@ elif(sys.argv[1]==str(0)):
     fileHD = "encodeHD.txt"
     fileHD_RNS = "encodeHD_RNS.txt"
     RNS_size = 2
-    total_bits_HDCompare = RNS_size*num_bits*polynomial_size
+    total_bits_HDCompare = 2*RNS_size*num_bits*polynomial_size
     num_coeff = int(polynomial_size*RNS_size)
     non_RNS = True
 
@@ -62,16 +68,32 @@ elif(sys.argv[1]==str(2)):
     fileHD = "encodeHD_nonNTT.txt"
     fileHD_RNS = "encodeHD_nonNTT_RNS.txt"
     RNS_size = 2
-    total_bits_HDCompare = RNS_size*num_bits*polynomial_size
+    total_bits_HDCompare = 2*RNS_size*num_bits*polynomial_size
     num_coeff = int(polynomial_size*RNS_size)
     extra = "_nonNTT"
     non_RNS = True
+
 
 elif(sys.argv[1]==str(3)):
     print("OpenFHE with no Optimizations")
     fileN2 = "encodeN2_nonOps_bounded.txt"
     fileHD = "encodeHD_nonOps.txt"
     extra = "_nonOps"
+
+
+elif(sys.argv[1]==str(4)):
+    print("OpenFHE with Optimizations multi RNS")
+    fileN2 = "encodeN2_multiRNS_bounded.txt"
+    fileHD = "encodeHD_multiRNS.txt"
+    fileHD_RNS = "encodeHD_multiRNS_RNSactive.txt"
+    extra = "_multiRNS"
+    RNS_size = 5
+    total_bits_HDCompare = 2*RNS_size*num_bits*polynomial_size
+    num_coeff = int(polynomial_size*RNS_size)
+    extra = "_multiRNS"
+    non_RNS = True
+    RNS_proportion = 4/5
+    RNS_proportion_ActiveRNS = 1/5
 
 if(len(sys.argv)>2):
     verbose = sys.argv[2]
@@ -83,7 +105,7 @@ if(ejecute):
     bycoeff_min = []
 
     encodingN2 = data_read(dir, fileN2, False, total_bits_HDCompare)
-    encodingHD = data_read(dir, fileHD, True, total_bits_HDCompare)
+    encodingHD = data_read(dir, fileHD, True, int(total_bits_HDCompare*RNS_proportion))
 
     y_label = 'L2 norm (\%)'
     N2_by_coeff_av, N2_by_bits_av, bycoeff_max, bycoeff_min, bybits_max, bybits_min  = data_reshape(encodingN2, num_coeff, num_bits, True, max_diff_tot)
@@ -138,7 +160,7 @@ if(ejecute):
     if(non_RNS):
         # Divido por 2 el total_bits, por que aca estoy mirando solo la mitad de la codificacion
         # Ya que solo me quede con una sola limb del RNS
-        encodingHD_RNS = data_read(dir, fileHD_RNS, True, int(total_bits_HDCompare/2))
+        encodingHD_RNS = data_read(dir, fileHD_RNS, True, int(total_bits_HDCompare*RNS_proportion_ActiveRNS))
         HD_by_coeff_av, HD_by_bits_av, bycoeff_max, bycoeff_min, bybits_max, bybits_min  = data_reshape(encodingHD_RNS, num_coeff, num_bits, False, max_diff_tot)
 
         plt.plot(HD_by_bits_av, color='green')
